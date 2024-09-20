fca7bf8ce2ae547bce9cf156176e5b00eafcb324
Luciano <luciano.lima.s@hotmail.com>
2024-09-20 20:46:50 -0300

    models da aplicação

--- ft_transcendence/apps/users/models.py
+++ ft_transcendence/apps/users/models.py
@@ -2,4 +2,23 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class Users(models.Model):
-    ...
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)  # Consider using Django's built-in User model for password management
+    nickname = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+    user_status = models.CharField(max_length=50, default='Ativo')
+
+    def __str__(self):
+        return self.nickname
+    
+class Friendship(models.Model):
+    user1 = models.ForeignKey(Users, related_name='friends1', on_delete=models.CASCADE)
+    user2 = models.ForeignKey(Users, related_name='friends2', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user1.nickname} and {self.user2.nickname}"
