7dcc21da3a64e9a8437eb77fcb293315e19aa90e
Luciano <luciano.lima.s@hotmail.com>
2024-09-26 15:51:32 -0300

    atualizando código

--- srcs/requirements/app/transcendence/apps/custom_auth/urls.py
+++ srcs/requirements/app/transcendence/apps/custom_auth/urls.py
@@ -5,10 +5,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),     #rota de cadastro
     path('', views.signin, name='signin'),     #rota de login
     path('signout/', views.signout, name='signout'),  #rota de logout
-    path('reset_password/', views.reset_password, name='reset_password'),    #rota de reset de senha
-    path('reset_password/<str:token>/', views.reset_password, name='reset_password'),    #rota de reset de senha
-    path('reset_password/<str:token>/<str:email>/', views.reset_password, name='reset_password'),    #rota de reset de senha
-    path('reset_password/<str:token>/<str:email>/<str:password>/', views.reset_password, name='reset_password'),    #rota de reset de senha
+    path('resetPassword/', views.resetPassword, name='resetPassword'),    #rota de reset de senha
+    path('resetPassword/<str:token>/', views.resetPassword, name='resetPassword'),    #rota de reset de senha
     path('recoverPassword/', views.recoverPassword, name='recoverPassword'),    #rota de recuperação de senha
     path('logado/', views.logado, name='logado'),    #rota de usuário logado
 ]
