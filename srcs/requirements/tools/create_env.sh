7198ff4727258ecfce63d90f8d73079a761866fc
Rodrigo <rodrigoadcarvalho@gmail.com>
2025-02-24 06:47:20 -0300

    review

--- srcs/requirements/tools/create_env.sh
+++ srcs/requirements/tools/create_env.sh
@@ -10,17 +10,14 @@ if [ -f "$ENV_FILE" ]; then
 fi
 
 cat <<EOL > "$ENV_FILE"
+COMPOSE_BAKE="true"
+
 SECRET_KEY="django-insecure-hy0f)6#-mkp1kq9+4o2dh!(uv=oa07yy&eaamu*y@6(di22rm("
 
-DJANGO_SETTINGS_MODULE=a_core.settings
+DJANGO_SETTINGS_MODULE="a_core.settings"
 
-# 0 False, 1 True
 DEBUG="1"
 
-#setup windows desktop environment
-WINDOWS="0"
-
-# Comma Separated values
 ALLOWED_HOSTS="transcendence, 127.0.0.1, localhost, app, *"
 CSRF_TRUSTED_ORIGINS="https://localhost, https://127.0.0.1"
 
@@ -39,7 +36,7 @@ PGADMIN_DEFAULT_PASSWORD="root"
 
 GF_SECURITY_ADMIN_USER="grafana"
 GF_SECURITY_ADMIN_PASSWORD="5432"
-GF_PATHS_PROVISIONING=/etc/grafana/provisioning
+GF_PATHS_PROVISIONING="/etc/grafana/provisioning"
 
 MINIO_ROOT_USER="minio"
 MINIO_ROOT_PASSWORD="87654321"
@@ -47,24 +44,24 @@ MINIO_BUCKET="transcendence"
 MINIO_ENDPOINT="minio:9000"
 MINIO_EXTERNAL_ENDPOINT="http://localhost:9002"
 
-EMAIL_API_KEY="Pegar no discord"
+EMAIL_API_KEY="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"
 DEFAULT_FROM_EMAIL="ft_transcendence@gmx.com"
 
 REDIS_HOST="redis"
 REDIS_PORT="6379"
 REDIS_ADDR="redis:6379"
 
-ES_JAVA_OPTS=-Xms512m -Xmx512m
-ES_MEM_LIMIT=1073741824
-KB_MEM_LIMIT=1073741824
-LICENSE=basic
-CLUSTER_NAME=es_cluster
-ELASTIC_USER=elastic
-ELASTIC_PASSWORD=must_be_string
-ELASTIC_HOSTS=https://elasticsearch:9200
+ES_JAVA_OPTS="-Xms512m -Xmx512m"
+ES_MEM_LIMIT="1073741824"
+KB_MEM_LIMIT="1073741824"
+LICENSE="basic"
+CLUSTER_NAME="es_cluster"
+ELASTIC_USER="elastic"
+ELASTIC_PASSWORD="must_be_string"
+ELASTIC_HOSTS="https://elasticsearch:9200"
 
-KIBANA_PASSWORD=must_be_string
-ENCRYPTION_KEY=c34d38b3a14956121ff2170e5030b471551370178f43e5626eec58b04a30fae2
+KIBANA_PASSWORD="must_be_string"
+ENCRYPTION_KEY="c34d38b3a14956121ff2170e5030b471551370178f43e5626eec58b04a30fae2"
 
 EOL
 
